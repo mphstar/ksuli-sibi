a4ecf87198d908b94b9e07b058ca49339397085f
mphstar <bintang.simdig20@gmail.com>
2025-02-22 18:40:29 +0700

    remove: component unused

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,6 @@ import { FaCircleCheck } from "react-icons/fa6";
 import useNavbarStore from "@/stores/NavbarStore";
 import MediapipeHelper from "@/helper/MediapipeHelper";
 import DetectionHelper from "@/helper/DetectionHelper";
-import MyLoading from "@/components/organisms/MyLoading";
 
 type PredictResult = {
   abjad: String;
